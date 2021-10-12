d5dc187c10b75b760485614dc1aefb84e62df4e6
VusalRA <vusalra@code.edu.az>
2021-10-12 20:15:53 +0400

    initial commit

---
+++ EventsApi/src/test/java/az/code/EventsApi/EventsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package az.code.EventsApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
